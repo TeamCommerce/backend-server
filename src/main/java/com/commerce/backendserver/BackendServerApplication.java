f3228daa2118aad42fd677c4e5af300fb7605df0
Haebin <hbyeon@student.42seoul.kr>
2023-08-04 16:41:31 +0900

    feat: init project files

---
+++ src/main/java/com/commerce/backendserver/BackendServerApplication.java
@@ -0,0 +1,13 @@
+package com.commerce.backendserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendServerApplication.class, args);
+	}
+
+}
