ec31b13ed7fc85ff8be57302df429c121c30bc23
EunChanNam <namhm23@gmail.com>
2023-08-09 13:25:50 +0900

    feat: ErrorResponse 정의

---
+++ src/main/java/com/commerce/backendserver/common/exception/error/ErrorResponse.java
@@ -0,0 +1,37 @@
+package com.commerce.backendserver.common.exception.error;
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+
+@Getter
+public class ErrorResponse {
+
+  private final String timeStamp;
+
+  private final int errorCode;
+
+  private final String errorMessage;
+
+  private ErrorResponse(int errorCode, String errorMessage) {
+    this.timeStamp = LocalDateTime.now().toString();
+    this.errorCode = errorCode;
+    this.errorMessage = errorMessage;
+  }
+
+  public static ErrorResponse of(ErrorCode errorCode) {
+    return new ErrorResponse(errorCode.getStatus().value(), errorCode.getMessage());
+  }
+
+  public static ErrorResponse of(HttpStatus httpStatus, String errorMessage) {
+    return new ErrorResponse(httpStatus.value(), errorMessage);
+  }
+
+  public static ErrorResponse of(HttpStatus httpStatus, FieldError fieldError) {
+    if (fieldError != null) {
+      return new ErrorResponse(httpStatus.value(), fieldError.getDefaultMessage());
+    }
+    return new ErrorResponse(httpStatus.value(), "Invalid Param");
+  }
+}
