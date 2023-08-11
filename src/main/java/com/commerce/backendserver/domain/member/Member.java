1032501ddd10f57e173c7c9147de72f56662215f
Haebin <hbyeon@student.42seoul.kr>
2023-08-11 17:38:18 +0900

    chore: Automated Deploy CD Pipeline (#25) (#26)

--- src/main/java/com/commerce/backendserver/domain/member/Member.java
+++ src/main/java/com/commerce/backendserver/domain/member/Member.java
@@ -1,10 +1,7 @@
 package com.commerce.backendserver.domain.member;
 
 import com.commerce.backendserver.domain.common.entity.BaseEntity;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import jakarta.persistence.*;
 
@@ -14,6 +11,7 @@ import static lombok.AccessLevel.PROTECTED;
 @Builder
 @Entity
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor(access = PROTECTED)
 @Table(name = "user_tb")
