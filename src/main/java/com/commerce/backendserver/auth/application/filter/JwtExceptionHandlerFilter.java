cb6bbb29f814536189476d6690d12bdf0e07a688
EunChanNam <75837025+EunChanNam@users.noreply.github.com>
2023-08-17 17:01:52 +0900

    feat: Add jwt logic and kakao, google oauth2 login (#35)
    * feat: Add jwt dependency
    
    * feat: implement JwtProvider
    
    * feat: Implement token entity
    
    * remove: 기존 Member 관련 코드 제거
    
    * feat: Member Entity 재정의
    
    * feat: Define OAuthProfile interface and Impl(GoogleProfile, KakaoProfile)
    
    * feat: Implement OAuthProfileFactory
    
    * feat: Implement OAuthLoginService
    
    * feat: Implement CustomOAuth2User, UserPrincipal and apply to OAuthLoginService
    
    * feat: Implement CustomOAuthLoginSuccessHandler
    
    * feat: Implement jwtAuthenticationFilter
    
    * feat: Implement JwtExceptionHandlerFilter
    
    * rename: Rename to CustomOAuth2LoginService
    
    * chore: move package
    
    * feat: Implement TokenRepository
    
    * feat: Define SecurityConfig
    
    * fix: OAuth 버그 픽스
    
    * chore: Update submodule version
    
    * remove: Remove logout handler class and config
    
    * feat: Add token upsert logic when login
    
    * chore: Rollback db config
    
    * refactor: Apply h-been's review
    
    * chore: Apply change of application-dev.yml

---
+++ src/main/java/com/commerce/backendserver/auth/application/filter/JwtExceptionHandlerFilter.java
@@ -0,0 +1,42 @@
+package com.commerce.backendserver.auth.application.filter;
+
+import com.commerce.backendserver.global.exception.CommerceException;
+import com.commerce.backendserver.global.exception.error.ErrorCode;
+import com.commerce.backendserver.global.exception.error.ErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtExceptionHandlerFilter extends OncePerRequestFilter {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain chain
+    ) throws ServletException, IOException {
+
+        try {
+            chain.doFilter(request, response);
+        } catch (CommerceException e) {
+            ErrorCode errorCode = e.getErrorCode();
+
+            response.setStatus(errorCode.getStatus().value());
+            response.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+            ErrorResponse errorResponse = ErrorResponse.of(errorCode);
+
+            objectMapper.writeValue(response.getOutputStream(), errorResponse);
+        }
+    }
+}
