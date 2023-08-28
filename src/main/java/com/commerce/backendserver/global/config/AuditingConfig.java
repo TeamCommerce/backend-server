b09c346f99b3d126598eea428825d9c5ad27c50d
EunChanNam <75837025+EunChanNam@users.noreply.github.com>
2023-08-29 01:00:26 +0900

    feat: Implement social login (#48)
    * feat: Add jwt logic and kakao, google oauth2 login (#35)
    
    * feat: Add jwt dependency
    
    * feat: implement JwtProvider
    
    * feat: Implement token entity
    
    * remove: 기존 Member 관련 코드 제거
    
    * feat: Member Entity 재정의
    
    * feat: Define OAuthProfile interface and Impl(GoogleProfile, KakaoProfile)
    
    * feat: Implement OAuthProfileFactory
    
    * feat: Implement OAuthLoginService
    
    * feat: Implement CustomOAuth2User, UserPrincipal and apply to OAuthLoginService
    
    * feat: Implement CustomOAuthLoginSuccessHandler
    
    * feat: Implement jwtAuthenticationFilter
    
    * feat: Implement JwtExceptionHandlerFilter
    
    * rename: Rename to CustomOAuth2LoginService
    
    * chore: move package
    
    * feat: Implement TokenRepository
    
    * feat: Define SecurityConfig
    
    * fix: OAuth 버그 픽스
    
    * chore: Update submodule version
    
    * remove: Remove logout handler class and config
    
    * feat: Add token upsert logic when login
    
    * chore: Rollback db config
    
    * refactor: Apply h-been's review
    
    * chore: Apply change of application-dev.yml
    
    * feat: Test infra layer of auth, domain layer of member and token (#36)
    
    * feat: Test construct method of member entity
    
    * feat: Define RepositoryTestBase
    
    * feat: Test MemberQueryRepository
    
    * rename: Rename Token constructor method
    
    * feat: Test Token entity
    
    * feat: Implement TokenUtils for testing
    
    * feat: Test MemberQueryRepository
    
    * rename: Rename displayName
    
    * feat: Test auth infra layer
    
    * chore: Update submodule
    
    * refactor: Refactor CustomOAuth2LoginService
    
    * fix: Fix JwtAuthenticationFilter bug
    
    * refactor: Apply h-been's code review
    
    * feat: Test JwtProvider and JwtAuthenticationFilter (#41)
    
    * refactor: Hide Token Validity in yml file
    
    * chore: Change package path
    
    * refactor: Remove empty line
    
    * feat: Test JwtProvider
    
    * chore: Update submodule
    
    * refactor: Transfer TestController inner class to class
    
    * rename: Rename displayname
    
    * feat: Test JwtFilters
    
    * refactor: Apply h-beeen's review
    
    * feat: Add test case
    
    * remove: Remove duplicated validation

---
+++ src/main/java/com/commerce/backendserver/global/config/AuditingConfig.java
@@ -0,0 +1,9 @@
+package com.commerce.backendserver.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditingConfig {
+}
---
+++ src/test/java/com/commerce/backendserver/auth/infra/oauth/OAuthProfileFactoryTest.java
@@ -0,0 +1,71 @@
+package com.commerce.backendserver.auth.infra.oauth;
+
+import com.commerce.backendserver.global.exception.CommerceException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.commerce.backendserver.auth.exception.AuthError.NOT_EXIST_OAUTH_TYPE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayName("[OAuthProfileFactory Test] (Infra layer)")
+class OAuthProfileFactoryTest {
+
+    private static final String GOOGLE = "google";
+    private static final String KAKAO = "kakao";
+    private static final String NONE = "none";
+
+    @Nested
+    @DisplayName("[loadOAuthProfile method]")
+    class LoadOAuthProfileTest {
+        //given
+        final OAuthProfileFactory factory = new OAuthProfileFactory(
+                List.of(new GoogleProfile(), new KakaoProfile())
+        );
+
+        @Test
+        @DisplayName("Google")
+        void google() {
+            //when
+            OAuthProfile result = factory.loadOAuthProfile(GOOGLE, null);
+
+            //then
+            assertThat(result).isInstanceOf(GoogleProfile.class);
+        }
+
+        @Test
+        @DisplayName("Kakao")
+        void kakao() {
+            //when
+            OAuthProfile result = factory.loadOAuthProfile(KAKAO, null);
+
+            //then
+            assertThat(result).isInstanceOf(KakaoProfile.class);
+        }
+
+        @Test
+        @DisplayName("Not supported type 1")
+        void failByNotSupportedType1() {
+            //when, then
+            assertThatThrownBy(() -> factory.loadOAuthProfile(NONE, null))
+                    .isInstanceOf(CommerceException.class)
+                    .hasMessageContaining(NOT_EXIST_OAUTH_TYPE.getMessage());
+        }
+
+        @Test
+        @DisplayName("Not supported type 2")
+        void failByNotSupportedType2() {
+            //given
+            OAuthProfileFactory factory = new OAuthProfileFactory(new ArrayList<>());
+
+            //when, then
+            assertThatThrownBy(() -> factory.loadOAuthProfile(GOOGLE, null))
+                    .isInstanceOf(CommerceException.class)
+                    .hasMessageContaining(NOT_EXIST_OAUTH_TYPE.getMessage());
+        }
+    }
+}
