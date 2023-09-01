ff7adbec217219047693f34703417afea739a771
EunChanNam <75837025+EunChanNam@users.noreply.github.com>
2023-09-01 17:14:45 +0900

    feat: Set up RestDocs (#52)
    * feat: Add RestDocs and Swagger config at build.gradle
    
    * chore: Update submodule
    
    * chore: Add RestDocs yaml file at gitignore
    
    * feat: Set up RestDocs
    
    * resolve: Resolve conflict

---
+++ src/test/java/com/commerce/backendserver/common/base/IntegrationTestBase.java
@@ -0,0 +1,44 @@
+package com.commerce.backendserver.common.base;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.snippet.Attributes;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.restassured.RestAssuredRestDocumentation.documentationConfiguration;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(RestDocumentationExtension.class)
+public abstract class IntegrationTestBase {
+
+    protected static final String DEFAULT_PATH = "{class_name}/{method_name}/";
+
+    protected RequestSpecification spec;
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUpRestDocs(RestDocumentationContextProvider restDocumentation) {
+        RestAssured.port = port;
+
+        this.spec = new RequestSpecBuilder()
+                .setPort(port)
+                .addFilter(documentationConfiguration(restDocumentation)
+                        .operationPreprocessors()
+                        .withRequestDefaults(prettyPrint())
+                        .withResponseDefaults(prettyPrint()))
+                .build();
+    }
+
+    protected Attributes.Attribute constraint(String value) {
+        return new Attributes.Attribute("constraints", value);
+    }
+}
