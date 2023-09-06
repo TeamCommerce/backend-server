c34ed429b57a4dd11858648651d90180056847c3
EunChanNam <namhm23@gmail.com>
2023-09-06 16:12:05 +0900

    feat: Implement review create api

---
+++ src/main/java/com/commerce/backendserver/review/presentation/ReviewController.java
@@ -0,0 +1,38 @@
+package com.commerce.backendserver.review.presentation;
+
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.commerce.backendserver.auth.infra.oauth.CustomOAuth2User;
+import com.commerce.backendserver.review.application.ReviewService;
+import com.commerce.backendserver.review.application.dto.request.CreateReviewRequest;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/reviews")
+@RequiredArgsConstructor
+public class ReviewController {
+
+	private final ReviewService reviewService;
+
+	@PostMapping
+	public ResponseEntity<Void> registerReview(
+		@Validated @RequestBody CreateReviewRequest request,
+		@AuthenticationPrincipal CustomOAuth2User member
+	) {
+		Long reviewId = reviewService.createReview(request, member.getId());
+
+		return ResponseEntity
+			.created(URI.create(String.format("/api/reviews/%s", reviewId)))
+			.build();
+	}
+}
+
