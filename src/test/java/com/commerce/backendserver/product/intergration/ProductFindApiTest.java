cd2e2b8555cf045fc7727fc0ae52906dd6bc3d2b
Haebin <hbyeon@student.42seoul.kr>
2023-09-12 04:32:01 +0900

    feat: Add TestCodes for RestDocs API + Swagger UI (#49)

--- src/test/java/com/commerce/backendserver/product/intergration/ProductFindApiTest.java
+++ src/test/java/com/commerce/backendserver/product/intergration/ProductFindApiTest.java
@@ -5,21 +5,26 @@ import com.commerce.backendserver.product.domain.Product;
 import com.commerce.backendserver.product.domain.persistence.ProductCommandRepository;
 import com.commerce.backendserver.product.domain.persistence.promotion.PromotionCommandRepository;
 import com.commerce.backendserver.product.domain.promotion.Promotion;
-import org.junit.jupiter.api.*;
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
+import com.epages.restdocs.apispec.ResourceSnippetParametersBuilder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.restdocs.snippet.Snippet;
-import org.springframework.test.annotation.Rollback;
 
 import static com.commerce.backendserver.product.fixture.ProductFixture.VALID_PRODUCT;
-import static com.commerce.backendserver.product.fixture.PromotionFixture.VALID_PROMOTION;
+import static com.commerce.backendserver.product.fixture.PromotionFixture.*;
 import static com.epages.restdocs.apispec.RestAssuredRestDocumentationWrapper.document;
+import static com.epages.restdocs.apispec.Schema.schema;
 import static io.restassured.RestAssured.given;
-import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.restdocs.payload.JsonFieldType.ARRAY;
 import static org.springframework.restdocs.payload.JsonFieldType.OBJECT;
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 
@@ -32,9 +37,9 @@ class ProductFindApiTest extends IntegrationTestBase {
     @Autowired
     private PromotionCommandRepository promotionCommandRepository;
 
+    Long id;
     Product savedProduct;
     Promotion savedPromotion;
-    Long id;
 
     @BeforeEach
     void initProduct() {
@@ -45,7 +50,19 @@ class ProductFindApiTest extends IntegrationTestBase {
 
     @Nested
     @DisplayName("[findSingleProduct] 단일 상품 기본 조회")
-    class findSingleProduct {
+    class 단일_상품_조회 {
+
+        private static ResourceSnippetParametersBuilder swaggerDescriptionWithResponseSchema(
+                String ResponseSchema
+        ) {
+            return ResourceSnippetParameters.builder()
+                    .tag("SingleProduct - 단일 상품 관련 API")
+                    .summary("단일 상품 조회 API - @h.beeen")
+                    .links()
+                    .description("상품 ID를 활용해 기본 정보(메인페이지 필요 정보)를 조회하는 API")
+                    .requestSchema(schema("[FindSingleProduct] Request - GET"))
+                    .responseSchema(schema(ResponseSchema));
+        }
 
         private static final Snippet PATH_PARAMETERS = pathParameters(
                 parameterWithName("id").description("조회할 상품 ID")
@@ -80,10 +97,19 @@ class ProductFindApiTest extends IntegrationTestBase {
                 fieldWithPath("options[].selectionOption.additionalFee").description("추가 비용")
         );
 
+        private static final Snippet ERROR_RESPONSE = responseFields(
+                fieldWithPath("timeStamp").description("에러 발생 시간"),
+                fieldWithPath("errorCode").description("발생 에러 httpStatus"),
+                fieldWithPath("errorMessage").description("에러 메세지")
+        );
+
         @Test
-        void When_ValidSingleProductPresented_Then_ReturnProductInfo() throws Exception {
+        @DisplayName("[Success] 상품 정보 조회 정상 성공")
+        void success_상품_정보_정상_조회에_성공한다() {
             given(spec)
-                    .filter(document(DEFAULT_PATH, PATH_PARAMETERS, RESPONSE_FIELDS))
+                    .filter(document(DEFAULT_PATH,
+                            swaggerDescriptionWithResponseSchema("[FindSingleResponse] FindSingleProductResponse (GET)"),
+                            PATH_PARAMETERS, RESPONSE_FIELDS))
                     .accept(APPLICATION_JSON_VALUE)
                     .header("Content-type", "application/json")
                     .log().all()
@@ -92,7 +118,75 @@ class ProductFindApiTest extends IntegrationTestBase {
                     .get("/api/products/{id}", savedProduct.getId())
 
                     .then().log().all()
-                    .statusCode(OK.value());
+                    .statusCode(OK.value())
+                    .extract();
+        }
+
+        @Test
+        @DisplayName("[Fail] 상품 정보 조회 실패")
+        void fail_존재하지_않는_상품_조회시_예외를_던진다() {
+            given(spec)
+                    .filter(document(DEFAULT_PATH,
+                            swaggerDescriptionWithResponseSchema("[FindSingleResponse] Member Not Found (404)"),
+                            PATH_PARAMETERS, ERROR_RESPONSE))
+                    .accept(APPLICATION_JSON_VALUE)
+                    .header("Content-type", "application/json")
+                    .log().all()
+
+                    .when().log().all()
+                    .get("/api/products/{id}", -2147483648)
+
+                    .then().log().all()
+                    .statusCode(NOT_FOUND.value())
+                    .extract();
+        }
+
+        @Test
+        @DisplayName("[Fail] 비정상 정액 프로모션(originPrice - discountedAmount < 0)")
+        void fail_정가보다_정액_할인_프로모션_가격이_크면_예외를_던진다() {
+            //given
+            Promotion tooHighPromotion = promotionCommandRepository.save(TOO_HIGH_PROMOTION.toEntity());
+            Product minusPriceProduct = productCommandRepository.save(VALID_PRODUCT.toEntity(tooHighPromotion));
+
+            //when && then
+            given(spec)
+                    .filter(document(DEFAULT_PATH,
+                            swaggerDescriptionWithResponseSchema("[FindSingleResponse] Bad Request (400)"),
+                            PATH_PARAMETERS, ERROR_RESPONSE))
+                    .accept(APPLICATION_JSON_VALUE)
+                    .header("Content-type", "application/json")
+                    .log().all()
+
+                    .when().log().all()
+                    .get("/api/products/{id}", minusPriceProduct.getId())
+
+                    .then().log().all()
+                    .statusCode(BAD_REQUEST.value())
+                    .extract();
+        }
+
+        @Test
+        @DisplayName("[Fail] 비정상 정액 프로모션(discountedAmount 음수")
+        void fail_정액_할인_프로모션_값이_음수면_예외를_던진다() {
+            //given
+            Promotion minusPromotion = promotionCommandRepository.save(MINUS_FIX_PROMOTION.toEntity());
+            Product product = productCommandRepository.save(VALID_PRODUCT.toEntity(minusPromotion));
+
+            //when && then
+            given(spec)
+                    .filter(document(DEFAULT_PATH,
+                            swaggerDescriptionWithResponseSchema("[FindSingleResponse] Bad Request (400)"),
+                            PATH_PARAMETERS, ERROR_RESPONSE))
+                    .accept(APPLICATION_JSON_VALUE)
+                    .header("Content-type", "application/json")
+                    .log().all()
+
+                    .when().log().all()
+                    .get("/api/products/{id}", product.getId())
+
+                    .then().log().all()
+                    .statusCode(BAD_REQUEST.value())
+                    .extract();
         }
     }
 }
