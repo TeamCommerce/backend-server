9421c12b1e642dfaacdd24d284d79310e2b6fda9
EunChanNam <namhm23@gmail.com>
2023-09-19 18:47:36 +0900

    feat: ProductOption 를 포함한 Product 단건 조회 쿼리 구현
    * id 를 통해 단건 조회
    * ProductOption 패치조인으로 패칭

--- src/main/java/com/commerce/backendserver/product/infra/persistence/ProductQueryRepository.java
+++ src/main/java/com/commerce/backendserver/product/infra/persistence/ProductQueryRepository.java
@@ -1,6 +1,8 @@
 package com.commerce.backendserver.product.infra.persistence;
 
 import com.commerce.backendserver.product.domain.Product;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,4 +17,7 @@ public interface ProductQueryRepository extends ProductJpaRepository, ProductQue
             "left join fetch p.images " +
             "where p.id = :id")
     Optional<Product> findProductInfoById(@Param("id") Long id);
+
+    @EntityGraph(attributePaths = "options")
+    Optional<Product> findWithOptionsById(Long id);
 }
