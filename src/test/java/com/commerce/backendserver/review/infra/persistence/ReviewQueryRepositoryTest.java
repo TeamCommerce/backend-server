5f716e256cd68746ef8b5a68c98452b0b8525b57
EunChanNam <namhm23@gmail.com>
2023-09-21 01:23:33 +0900

    feat: 리뷰 findReviewByStatisticCondition 쿼리 테스팅
    * 입력 조건별로 테스팅

---
+++ src/test/java/com/commerce/backendserver/review/infra/persistence/ReviewQueryRepositoryTest.java
@@ -0,0 +1,231 @@
+package com.commerce.backendserver.review.infra.persistence;
+
+import static com.commerce.backendserver.product.domain.option.constants.ProductSize.*;
+import static com.commerce.backendserver.product.fixture.ProductFixture.*;
+import static com.commerce.backendserver.product.fixture.PromotionFixture.*;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.commerce.backendserver.common.base.RepositoryTestBase;
+import com.commerce.backendserver.common.fixture.ReviewFixture;
+import com.commerce.backendserver.product.domain.Product;
+import com.commerce.backendserver.product.domain.option.constants.ProductSize;
+import com.commerce.backendserver.product.domain.promotion.Promotion;
+import com.commerce.backendserver.product.infra.persistence.ProductCommandRepository;
+import com.commerce.backendserver.product.infra.persistence.promotion.PromotionCommandRepository;
+import com.commerce.backendserver.review.domain.Review;
+import com.commerce.backendserver.review.domain.additionalinfo.AdditionalInfo;
+
+@DisplayName("[ReviewQueryRepository Test] - Infra layer")
+class ReviewQueryRepositoryTest extends RepositoryTestBase {
+
+	@Autowired
+	private ReviewQueryRepository reviewQueryRepository;
+
+	@Autowired
+	private ProductCommandRepository productRepository;
+
+	@Autowired
+	private PromotionCommandRepository promotionRepository;
+
+	private final Set<Review> reviews = new HashSet<>();
+
+	private List<Product> products;
+
+	@BeforeEach
+	void setUp() {
+		Promotion promotion = promotionRepository.save(VALID_RATE_PROMOTION.toEntity());
+
+		products = List.of(
+			productRepository.save(VALID_PRODUCT.toEntity(promotion)),
+			productRepository.save(VALID_PRODUCT_2.toEntity(promotion)),
+			productRepository.save(VALID_PRODUCT_3.toEntity(promotion))
+		);
+
+		ReviewFixture[] reviewFixtures = ReviewFixture.values();
+		for (int i = 0; i < products.size(); i++) {
+			Product product = products.get(i);
+			Long optionId = product.getOptions().get(0).getId();
+
+			reviews.add(reviewQueryRepository.save(reviewFixtures[i].toEntity(product, 1L, optionId)));
+		}
+	}
+
+	@Nested
+	@DisplayName("[findReviewByStatisticCondition]")
+	class findReviewByStatisticConditionTest {
+
+		@Test
+		@DisplayName("[Success] 조건이 없을때 모든 리뷰를 조회한다")
+		void successWhenNoCondition() {
+			//when
+			List<Review> result = reviewQueryRepository.findReviewByStatisticCondition(
+				null, null, null, null, null
+			);
+
+			//then
+			assertThat(result).containsAll(reviews);
+		}
+
+		@Test
+		@DisplayName("[Success] color 에 대한 조건에 맞는 리뷰를 조회한다")
+		void successWhenHasColorCondition() {
+			//given
+			Set<String> engColorNames = Set.of("blue", "red");
+
+			//when
+			List<Review> result = reviewQueryRepository.findReviewByStatisticCondition(
+				engColorNames, null, null, null, null
+			);
+
+			//then
+			assertThat(result).hasSize(2);
+
+			List<Long> expectedOptionIds = generateExpectedOptionIds(
+				product -> {
+					String engColorName = product.getOptions().get(0).getColor().getEngColorName();
+					return engColorNames.contains(engColorName);
+				});
+
+			List<Review> expectedReviews = fetchExpectedReviews(expectedOptionIds);
+
+			assertThat(result).containsAll(expectedReviews);
+		}
+
+		@Test
+		@DisplayName("[Success] size 에 대한 조건에 맞는 리뷰를 조회한다")
+		void successWhenHasSizeCondition() {
+			//given
+			Set<ProductSize> sizes = Set.of(TWO_XS, L);
+
+			//when
+			List<Review> result = reviewQueryRepository.findReviewByStatisticCondition(
+				null, sizes, null, null, null
+			);
+
+			//then
+			assertQueryResultByProductCondition(
+				product -> sizes.contains(product.getOptions().get(0).getSize()),
+				result,
+				2
+			);
+		}
+
+		@Test
+		@DisplayName("[Success] additionalOption 에 대한 조건에 맞는 리뷰를 조회한다")
+		void successWhenHasAdditionalOptionCondition() {
+			//given
+			Set<String> additionalOptionValues = Set.of("O");
+
+			//when
+			List<Review> result = reviewQueryRepository.findReviewByStatisticCondition(
+				null, null, additionalOptionValues, null, null
+			);
+
+			//then
+			assertQueryResultByProductCondition(
+				product -> {
+					String optionValue = product.getOptions().get(0).getSelectionOption().getValue();
+					return additionalOptionValues.contains(optionValue);
+				},
+				result,
+				2
+			);
+		}
+
+		@Test
+		@DisplayName("[Success] score 에 대한 조건에 맞는 리뷰를 조회한다")
+		void successWhenHasScoreCondition() {
+			//given
+			Set<Integer> scores = Set.of(4, 2);
+
+			//when
+			List<Review> result = reviewQueryRepository.findReviewByStatisticCondition(
+				null, null, null, scores, null
+			);
+
+			//then
+			assertQueryResultByReviewCondition(
+				review -> scores.contains(review.getScore()),
+				result,
+				2
+			);
+		}
+
+		@Test
+		@DisplayName("[Success] additionalInfo 에 대한 조건에 맞는 리뷰를 조회한다")
+		void successWhenHasAdditionalInfoCondition() {
+			//given
+			Set<String> additionalInfoValues = Set.of("Large", "Small");
+
+			//when
+			List<Review> result = reviewQueryRepository.findReviewByStatisticCondition(
+				null, null, null, null, additionalInfoValues
+			);
+
+			//then
+			assertQueryResultByReviewCondition(
+				review -> {
+					List<String> targetInfoValues = review.getAdditionalInfoList().stream()
+						.map(AdditionalInfo::getInfoValue).toList();
+					return targetInfoValues.stream().anyMatch(additionalInfoValues::contains);
+				},
+				result,
+				2
+			);
+		}
+
+		private void assertQueryResultByProductCondition(
+			final Predicate<Product> hasConditionCommand,
+			final List<Review> result,
+			final int expectedSize
+		) {
+			assertThat(result).hasSize(expectedSize);
+
+			List<Long> expectedOptionIds = generateExpectedOptionIds(hasConditionCommand);
+
+			List<Review> expectedReviews = fetchExpectedReviews(expectedOptionIds);
+
+			assertThat(result).containsAll(expectedReviews);
+		}
+
+		private void assertQueryResultByReviewCondition(
+			final Predicate<Review> hasConditionCommand,
+			final List<Review> result,
+			final int expectedSize
+		) {
+			assertThat(result).hasSize(expectedSize);
+
+			List<Review> expectedReviews = reviews.stream()
+				.filter(hasConditionCommand)
+				.toList();
+
+			assertThat(result).containsAll(expectedReviews);
+		}
+	}
+
+	private List<Review> fetchExpectedReviews(List<Long> expectedOptionIds) {
+		return reviews.stream()
+			.filter(review -> expectedOptionIds.contains(review.getProductOptionId()))
+			.toList();
+	}
+
+	private List<Long> generateExpectedOptionIds(
+		final Predicate<Product> hasConditionCommand
+	) {
+		return products.stream()
+			.filter(hasConditionCommand)
+			.map(product -> product.getOptions().get(0).getId())
+			.toList();
+	}
+}
