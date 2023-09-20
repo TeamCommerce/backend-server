aa200ee48a5fc9b6550e31070103727740f347bb
EunChanNam <75837025+EunChanNam@users.noreply.github.com>
2023-09-20 13:14:55 +0900

    feat: 리뷰 등록 서비스 로직에 ProductOptionId 검증 로직 추가 (#88)
    * feat: ProductOption 를 포함한 Product 단건 조회 쿼리 구현
    
    * id 를 통해 단건 조회
    * ProductOption 패치조인으로 패칭
    
    * feat: 리뷰 등록 서비스로직에 ProductOptionId 검증로직 추가
    
    * 검증 메소드 :  checkMatchingProductOptionIdToProduct
    
    * feat: 상품 findWithOptionsById 쿼리 테스팅
    
    * 성공
    * 입력 id 에 맞는 상품이 없을 경우 실패
    
    * feat: 리뷰 등록 서비스 로직의 ProductOptionId 검증로직 테스팅
    
    * rename: 기존 Product 쿼리 findWithOptionsById -> findDistinctWithOptionsById 으로 변경

--- src/main/java/com/commerce/backendserver/review/exception/ReviewError.java
+++ src/main/java/com/commerce/backendserver/review/exception/ReviewError.java
@@ -14,7 +14,8 @@ public enum ReviewError implements ErrorCode {
     INVALID_LENGTH("최소 5자 이상으로 작성해주세요.", BAD_REQUEST, "R_001"),
     NOT_EXIST_INFO_NAME("존재하지 않는 추가 정보 이름입니다.", BAD_REQUEST, "R_002"),
     INVALID_INTEGER_INFO_VALUE("숫자 관련 추가 정보에는 숫자만 입력해주세요.", BAD_REQUEST, "R_003"),
-    INVALID_ADDITIONAL_INFO("잘못된 형식의 추가정보 입니다.", BAD_REQUEST, "R_004")
+    INVALID_ADDITIONAL_INFO("잘못된 형식의 추가정보 입니다.", BAD_REQUEST, "R_004"),
+    NOT_MATCH_PRODUCT_OPTION_ID("상품 옵션이 상품에 존재하지 않습니다.", BAD_REQUEST, "R_005"),
     ;
 
     private final String message;
