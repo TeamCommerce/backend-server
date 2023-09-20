9a7ef0f7f010fa904412a0992b814ca1408b70a0
Haebin <hbyeon@student.42seoul.kr>
2023-09-20 17:12:22 +0900

    fix: Modify Product API Response Spec

--- src/main/java/com/commerce/backendserver/product/application/dto/response/ProductOptionResponse.java
+++ src/main/java/com/commerce/backendserver/product/application/dto/response/ProductOptionResponse.java
@@ -2,11 +2,8 @@ package com.commerce.backendserver.product.application.dto.response;
 
 import com.commerce.backendserver.product.domain.option.ProductAdditionalOption;
 import com.commerce.backendserver.product.domain.option.ProductColor;
-import com.commerce.backendserver.product.domain.option.ProductOption;
 import com.commerce.backendserver.product.domain.option.constants.ProductSize;
 import com.commerce.backendserver.product.domain.option.constants.ProductStatus;
-
-import java.util.List;
 
 public record ProductOptionResponse(
         Long optionId,
@@ -16,20 +13,4 @@ public record ProductOptionResponse(
         ProductStatus status,
         ProductSize size
 ) {
-    public static List<ProductOptionResponse> toOptionResponseList(List<ProductOption> productOptions) {
-        return productOptions.stream()
-                .map(ProductOptionResponse::from)
-                .toList();
-    }
-
-    private static ProductOptionResponse from(ProductOption productOption) {
-        return new ProductOptionResponse(
-                productOption.getId(),
-                productOption.getColor(),
-                productOption.getAdditionalOption(),
-                productOption.getInventory(),
-                productOption.getStatus(),
-                productOption.getSize()
-        );
-    }
 }
