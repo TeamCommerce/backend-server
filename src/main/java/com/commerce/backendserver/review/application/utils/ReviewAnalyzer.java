2a946ecc4fd3451fd635c5b669642d0e15f4136e
EunChanNam <namhm23@gmail.com>
2023-09-21 18:31:44 +0900

    feat: ReviewAnalyzer 구현
    * 리뷰의 총 통계 분석을 담당

---
+++ src/main/java/com/commerce/backendserver/review/application/utils/ReviewAnalyzer.java
@@ -0,0 +1,63 @@
+package com.commerce.backendserver.review.application.utils;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+import com.commerce.backendserver.review.application.dto.response.RatioStatistic;
+import com.commerce.backendserver.review.application.dto.response.ReviewStatistics;
+import com.commerce.backendserver.review.domain.Review;
+import com.commerce.backendserver.review.domain.additionalinfo.AdditionalInfo;
+import com.commerce.backendserver.review.domain.additionalinfo.constants.InfoName;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class ReviewAnalyzer {
+
+	private static final String SIZE = InfoName.SIZE.getValue();
+
+	private final RatioAnalyzer ratioAnalyzer;
+
+	public ReviewStatistics analyzeReview(List<Review> reviews) {
+		List<Integer> scoreData = toScoreData(reviews);
+		List<List<AdditionalInfo>> additionalInfoData = toAdditionalInfoData(reviews);
+
+		Map<String, RatioStatistic> scoreStatistic = ratioAnalyzer.analyzeScore(scoreData);
+
+		Map<String, Map<String, RatioStatistic>> additionalInfoStatistic =
+			ratioAnalyzer.analyzeAdditionalInfo(additionalInfoData);
+
+		int totalReviewCount = reviews.size();
+
+		return new ReviewStatistics(
+			totalReviewCount,
+			calculateAverage(totalReviewCount, calculateTotalScore(scoreData)),
+			additionalInfoStatistic.get(SIZE).keySet(),
+			scoreStatistic,
+			additionalInfoStatistic
+		);
+	}
+
+	private int calculateTotalScore(List<Integer> scoreData) {
+		return scoreData.stream().mapToInt(i -> i).sum();
+	}
+
+	private List<List<AdditionalInfo>> toAdditionalInfoData(List<Review> reviews) {
+		return reviews.stream()
+			.map(Review::getAdditionalInfoList)
+			.toList();
+	}
+
+	private List<Integer> toScoreData(List<Review> reviews) {
+		return reviews.stream()
+			.map(Review::getScore)
+			.toList();
+	}
+
+	private double calculateAverage(double size, double sum) {
+		return sum / size;
+	}
+}
