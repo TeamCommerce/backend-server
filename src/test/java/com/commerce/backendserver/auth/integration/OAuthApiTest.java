2968b4fc66dbb701b031c9f3b97d4a1e1a061109
EunChanNam <namhm23@gmail.com>
2023-10-04 19:13:12 +0900

    feat: 소셜로그인 API 테스트 및 명세

--- src/test/java/com/commerce/backendserver/auth/integration/OAuthApiTest.java
+++ src/test/java/com/commerce/backendserver/auth/integration/OAuthApiTest.java
@@ -1,23 +1,41 @@
 package com.commerce.backendserver.auth.integration;
 
 import static com.commerce.backendserver.auth.integration.fixture.OAuthRequestFixture.*;
+import static com.commerce.backendserver.common.utils.TokenUtils.*;
 import static com.epages.restdocs.apispec.RestAssuredRestDocumentationWrapper.*;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
 import static org.springframework.restdocs.request.RequestDocumentation.*;
 
 import java.util.Set;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.restdocs.restassured.RestDocumentationFilter;
+import org.springframework.web.client.RestTemplate;
 
+import com.commerce.backendserver.auth.domain.model.OAuthMemberInfo;
+import com.commerce.backendserver.auth.domain.model.OAuthTokenInfo;
+import com.commerce.backendserver.auth.infra.oauth.google.model.GoogleMemberInfo;
+import com.commerce.backendserver.auth.infra.oauth.google.model.GoogleTokenInfo;
 import com.commerce.backendserver.common.base.IntegrationTestBase;
 
 import io.restassured.response.ValidatableResponse;
 
 @DisplayName("[OAuthApi Test] - Integration")
 class OAuthApiTest extends IntegrationTestBase {
+
+	@MockBean
+	private RestTemplate restTemplate;
 
 	@Nested
 	@DisplayName("[redirectToOAuthAuthorization]")
@@ -41,6 +59,52 @@ class OAuthApiTest extends IntegrationTestBase {
 			);
 
 			response.statusCode(HttpStatus.FOUND.value());
+		}
+	}
+
+	@Nested
+	@DisplayName("[login]")
+	class 소셜로그인 {
+
+		@BeforeEach
+		void setUp() {
+			OAuthTokenInfo oAuthTokenInfo = new GoogleTokenInfo(null, ACCESS_TOKEN, null, 10L);
+			OAuthMemberInfo oauthMemberInfo = new GoogleMemberInfo("id", "name", "email");
+
+			given(restTemplate.exchange(
+				anyString(),
+				any(HttpMethod.class),
+				any(HttpEntity.class),
+				any(Class.class)
+			)).willReturn(ResponseEntity.ok(oauthMemberInfo));
+
+			given(restTemplate.postForObject(
+				anyString(),
+				any(HttpEntity.class),
+				any())
+			).willReturn(oAuthTokenInfo);
+		}
+
+		@Test
+		@DisplayName("[Success] 구글 정보를 통해서 로그인한다")
+		void 구글() {
+			ValidatableResponse response = 구글_정보를_통해_로그인한다(spec);
+
+			response
+				.statusCode(200)
+				.body("accessToken", notNullValue())
+				.body("refreshToken", notNullValue());
+		}
+
+		@Test
+		@DisplayName("[Success] 카카오 정보를 통해서 로그인한다")
+		void 카카오() {
+			ValidatableResponse response = 카카오_정보를_통해_로그인한다(spec);
+
+			response
+				.statusCode(200)
+				.body("accessToken", notNullValue())
+				.body("refreshToken", notNullValue());
 		}
 	}
 
--- src/test/java/com/commerce/backendserver/auth/integration/fixture/OAuthRequestFixture.java
+++ src/test/java/com/commerce/backendserver/auth/integration/fixture/OAuthRequestFixture.java
@@ -1,12 +1,13 @@
 package com.commerce.backendserver.auth.integration.fixture;
 
+import static com.commerce.backendserver.common.fixture.CommonRequestFixture.*;
 import static lombok.AccessLevel.*;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.restdocs.restassured.RestDocumentationFilter;
-
-import com.commerce.backendserver.common.fixture.CommonRequestFixture;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import io.restassured.RestAssured;
 import io.restassured.response.ValidatableResponse;
@@ -16,7 +17,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = PRIVATE)
 public final class OAuthRequestFixture {
 
-	private static final String oauthPath = "/api/oauth/{provider}";
+	private static final String OAUTH_PATH = "/api/oauth/{provider}";
+	private static final String LOGIN_PATH = "/login/oauth2/code/{provider}";
 
 	public static ValidatableResponse 카카오_소셜로그인_페이지로_이동한다(
 		final RequestSpecification spec,
@@ -32,11 +34,43 @@ public final class OAuthRequestFixture {
 		return getOAuthRedirectRequest(spec, documentations, "google");
 	}
 
+	public static ValidatableResponse 구글_정보를_통해_로그인한다(
+		final RequestSpecification spec
+	) {
+		return getOAuthLoginRequest(spec, "google");
+	}
+
+	public static ValidatableResponse 카카오_정보를_통해_로그인한다(
+		final RequestSpecification spec
+	) {
+		return getOAuthLoginRequest(spec, "kakao");
+	}
+
+	private static ValidatableResponse getOAuthLoginRequest(RequestSpecification spec, String provider) {
+		String path = UriComponentsBuilder
+			.fromUriString(LOGIN_PATH)
+			.queryParam("code", "code")
+			.queryParam("state", "state")
+			.toUriString().replace("%7B", "{").replace("%7D", "}");
+
+		return getRequest(
+			RestAssured.given(spec).log().all(),
+			new HashSet<>(),
+			path,
+			provider
+		);
+	}
+
 	private static ValidatableResponse getOAuthRedirectRequest(
 		final RequestSpecification spec,
 		final Set<RestDocumentationFilter> documentations,
-		final String google
+		final String provider
 	) {
-		return CommonRequestFixture.getRequest(RestAssured.given(spec), documentations, oauthPath, google);
+		return getRequest(
+			RestAssured.given(spec),
+			documentations,
+			OAUTH_PATH,
+			provider
+		);
 	}
 }
