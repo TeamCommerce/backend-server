e4e55d2e7b3399f665702bbc2a21eca5d754b9b1
Haebin <hbyeon@student.42seoul.kr>
2023-10-08 13:27:35 +0900

    feat: remove @Param Annotation

--- src/main/java/com/commerce/backendserver/product/domain/ProductRepository.java
+++ src/main/java/com/commerce/backendserver/product/domain/ProductRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 public interface ProductRepository {
 
     // Query
-    Optional<Product> findProductInfoById(@Param("id") Long id);
+    Optional<Product> findProductInfoById(Long id);
 
     List<Product> findBestProducts();
 
