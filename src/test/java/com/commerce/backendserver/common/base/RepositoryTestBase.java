2107978bed76af373a1ceac7ceadba718a565ace
EunChanNam <namhm23@gmail.com>
2023-10-08 19:56:26 +0900

    refactor: 리뷰 레포지토리 관련 테스트코드 의존성 수정

--- src/test/java/com/commerce/backendserver/common/base/RepositoryTestBase.java
+++ src/test/java/com/commerce/backendserver/common/base/RepositoryTestBase.java
@@ -4,15 +4,16 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Repository;
 
-import com.commerce.backendserver.global.config.AuditingConfig;
-import com.commerce.backendserver.global.config.QueryDslConfig;
+import com.commerce.backendserver.common.config.QueryDslConfig;
 
 @Import(QueryDslConfig.class)
 @DataJpaTest(
 	includeFilters = @ComponentScan.Filter(
-		type = FilterType.ASSIGNABLE_TYPE,
-		classes = AuditingConfig.class
-	))
+		type = FilterType.ANNOTATION,
+		classes = Repository.class
+	)
+)
 public abstract class RepositoryTestBase {
 }
