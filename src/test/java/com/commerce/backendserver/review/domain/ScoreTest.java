8fcea2e63e3fa2473af04b2dffd6e0f01db8b4d8
EunChanNam <namhm23@gmail.com>
2023-10-08 20:07:28 +0900

    feat: Score 검증 테스트케이스 추가

--- src/test/java/com/commerce/backendserver/review/domain/ScoreTest.java
+++ src/test/java/com/commerce/backendserver/review/domain/ScoreTest.java
@@ -1,10 +1,14 @@
 package com.commerce.backendserver.review.domain;
 
+import static com.commerce.backendserver.review.exception.ReviewError.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.ThrowableAssert.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import com.commerce.backendserver.global.exception.CommerceException;
 
 @DisplayName("[Score Test] - Domain layer")
 class ScoreTest {
@@ -18,12 +22,27 @@ class ScoreTest {
 		void success() {
 			//given
 			final int score = 3;
-			
+
 			//when
 			Score result = new Score(score);
 
 			//then
 			assertThat(result.getValue()).isEqualTo(score);
+		}
+
+		@Test
+		@DisplayName("[Fail] 점수가 범위 밖에 있어 생성에 실패한다")
+		void failWhenScoreOutOfRange() {
+			//given
+			final int score = 9;
+
+			//when
+			ThrowingCallable when = () -> new Score(score);
+
+			//then
+			assertThatThrownBy(when)
+				.isInstanceOf(CommerceException.class)
+				.hasMessageContaining(INVALID_RANGE_SCORE.getMessage());
 		}
 	}
 }
